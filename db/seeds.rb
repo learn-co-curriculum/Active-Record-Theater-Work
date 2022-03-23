81a317e5d7c0de926660f0e140e082471e49aa1c
Eric Van <ggclarence@yahoo.com>
2022-03-22 20:44:35 -0500

    a

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+require 'pry'
+puts "Seeding database..."
+50.times do
+    role = Role.create(character_name: Faker::Name.name)
+    rand(1..10).times do
+        Audition.create(
+            
+            actor: Faker::Name.name,
+            location: Faker::Address.city,
+            phone: Faker::Number.number(10),
+            hired: Faker::Boolean.boolean,
+            role_id: role.id
+        )
+    end
+end
+puts "Database seeded!"
