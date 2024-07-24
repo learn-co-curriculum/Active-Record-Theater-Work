1c14ec99fa0b83aacb07535454a36cac821abff6
Stephen McKeon <stephen.mckeon@powerhrg.com>
2024-07-24 09:20:13 -0400

    Return locations and lead actor

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,4 +4,12 @@ class Role < ActiveRecord::Base
   def actors
     auditions.pluck(:actor)
   end
+
+  def locations
+    auditions.pluck(:location)
+  end
+
+  def lead
+    auditions.find_by(hired: true) || "no actor has been hired for this role"
+  end
 end
