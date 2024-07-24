b856aafc9314929f05227c71d46cbbdb599f3766
Stephen McKeon <stephen.mckeon@powerhrg.com>
2024-07-24 13:50:23 -0400

    Return actor with most roles

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,11 @@
 class Actor < ActiveRecord::Base
   has_many :auditions
   has_many :roles, through: :auditions
+
+  def self.most_roles
+    joins(:auditions)
+      .group("actors.id")
+      .order("COUNT(actors.id) DESC")
+      .first
+  end
 end
